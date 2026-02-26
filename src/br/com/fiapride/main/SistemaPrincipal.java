053c49bf2f64b914b0368873cb9877bfc1c20556
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-26 11:10:02 -0300

    feat: adiciona métodos de comportamento e atualiza README

--- src/br/com/fiapride/main/SistemaPrincipal.java
+++ src/br/com/fiapride/main/SistemaPrincipal.java
@@ -1,17 +1,21 @@
 package br.com.fiapride.main;
 
 import br.com.fiapride.model.Cachorro;
+import java.util.Scanner;
 
 public class SistemaPrincipal {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Cachorro cachorro1 = new Cachorro("Billy", "Vira-lata", 2);
+        System.out.print("Digite kms total andados: ");
+        Double n1 = scanner.nextDouble();
+        System.out.print("Digite o total de comida: ");
+        Double n2 = scanner.nextDouble();
+        cachorro1.andar(n1);
 
-        Cachorro cachorro1 = new Cachorro();
-        cachorro1.nome = "Billy";
-        cachorro1.raca = "Spitz";
-        cachorro1.idade = 9;
+        cachorro1.comer(n2);
 
-        System.out.println("--- Sistema FiapRide ---");
-        System.out.println("Nome " + cachorro1.nome + " | Raça: " + cachorro1.raca + " | Idade: " + cachorro1.idade);
+        scanner.close();
     }
 }
--- src/br/com/fiapride/model/Cachorro.java
+++ src/br/com/fiapride/model/Cachorro.java
@@ -4,4 +4,31 @@ public class Cachorro {
     public String nome;
     public String raca;
     public int idade;
+    public double kms;
+    public double gr;
+
+    public Cachorro(String nome, String raca, int idade){
+        this.nome = nome;
+        this.raca = raca;
+        this.idade = idade;
+        this.kms = 0.0;
+        this.gr = 0.0;
+    }
+    public void andar(double km){
+        if (km<= 0){
+            System.out.println("Valor deve ser maior que 0.");
+            return;
+        }
+        this.kms += km;
+        System.out.println("Total de KMs andados: " + this.kms);
+    }
+
+    public void comer(double gramas){
+        if (gramas<= 0){
+            System.out.println("Valor deve ser maior que 0.");
+            return;
+        }
+        this.gr += gramas;
+        System.out.println("Total de ração comida: " + this.gr);
+    }
 }
