9f5f023c5243ab55b6e2d6c567a6992851419245
Mitsuo10 <pedro.risardi@gmail.com>
2026-03-25 16:12:41 -0300

    Sistema Principal atualizado

--- src/br/com/fiapride/main/SistemaPrincipal.java
+++ src/br/com/fiapride/main/SistemaPrincipal.java
@@ -8,15 +8,34 @@ public class SistemaPrincipal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Cachorro cachorro1 = new Cachorro("Billy", "Vira-lata", 2);
+
+        System.out.print("Digite o novo nome: ");
+        String nome = scanner.next();
+
+        System.out.print("Digite a nova raça: ");
+        String raca = scanner.next();
+
+        System.out.print("Digite a nova idade: ");
+        int idade = scanner.nextInt();
+
         System.out.print("Digite kms total andados: ");
-        Double n1 = scanner.nextDouble();
+        double kms = scanner.nextDouble();
+
         System.out.print("Digite o total de comida: ");
-        Double n2 = scanner.nextDouble();
-        System.out.println("Cachorro " + cachorro1.nome + " de raça " + cachorro1.raca + " e idade " + cachorro1.idade + " anos:");
+        double comida = scanner.nextDouble();
 
-        cachorro1.andar(n1);
+        cachorro1.setNome(nome);
+        cachorro1.setRaca(raca);
+        cachorro1.setIdade(idade);
+        cachorro1.setKms(kms);
+        cachorro1.setGr(comida);
 
-        cachorro1.comer(n2);
+        System.out.println("\nCachorro atualizado:");
+        System.out.println("Nome: " + cachorro1.getNome());
+        System.out.println("Raça: " + cachorro1.getRaca());
+        System.out.println("Idade: " + cachorro1.getIdade());
+        System.out.println("KMs: " + cachorro1.getKms());
+        System.out.println("Comida: " + cachorro1.getGr());
 
         scanner.close();
     }
--- src/br/com/fiapride/model/Cachorro.java
+++ src/br/com/fiapride/model/Cachorro.java
@@ -1,11 +1,11 @@
 package br.com.fiapride.model;
 
 public class Cachorro {
-    public String nome;
-    public String raca;
-    public int idade;
-    public double kms;
-    public double gr;
+    private String nome;
+    private String raca;
+    private int idade;
+    private double kms;
+    private double gr;
 
     public Cachorro(String nome, String raca, int idade){
         this.nome = nome;
@@ -14,8 +14,9 @@ public class Cachorro {
         this.kms = 0.0;
         this.gr = 0.0;
     }
+
     public void andar(double km){
-        if (km<= 0){
+        if (km <= 0){
             System.out.println("Valor deve ser maior que 0.");
             return;
         }
@@ -24,11 +25,63 @@ public class Cachorro {
     }
 
     public void comer(double gramas){
-        if (gramas<= 0){
+        if (gramas <= 0){
             System.out.println("Valor deve ser maior que 0.");
             return;
         }
         this.gr += gramas;
         System.out.println("Total de ração comida: " + this.gr);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getKms() {
+        return kms;
+    }
+
+    public double getGr() {
+        return gr;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public void setIdade(int idade) {
+        if (idade < 0) {
+            System.out.println("Idade inválida.");
+            return;
+        }
+        this.idade = idade;
+    }
+
+    public void setKms(double kms) {
+        if (kms < 0) {
+            System.out.println("Valor inválido.");
+            return;
+        }
+        this.kms = kms;
+    }
+
+    public void setGr(double gr) {
+        if (gr < 0) {
+            System.out.println("Valor inválido.");
+            return;
+        }
+        this.gr = gr;
     }
 }
